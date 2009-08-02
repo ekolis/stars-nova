95bb20e078142dbd726dda2f140b99fdec12a450
evild00d <evild00d@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2009-08-02 18:34:14 +0000

    Commented out Chameleon tech for Jack of all Trades and fixed typo on Mini-Coloniser for Hyper-Expansion.

--- trunk/Nova GUI/GUIdata.cs
+++ trunk/Nova GUI/GUIdata.cs
@@ -28,242 +28,251 @@ using System;
 namespace Nova
 {
 
-// ============================================================================
-// Initialise all game data
-// ============================================================================
+    // ============================================================================
+    // Initialise all game data
+    // ============================================================================
 
-   public static class GUIdata
-   {
-      private static BinaryFormatter Formatter = new BinaryFormatter();
+    public static class GUIdata
+    {
+        private static BinaryFormatter Formatter = new BinaryFormatter();
 
-// ============================================================================
-// Initialise the data needed for the GUI to run.
-// On first run a password is required , on any additional turnsb no password 
-// is required.
-// ============================================================================
+        // ============================================================================
+        // Initialise the data needed for the GUI to run.
+        // On first run a password is required , on any additional turnsb no password 
+        // is required.
+        // ============================================================================
 
-      public static void Load(bool bPasswordNeeded)
-      {
-         //GUIstate.Data = GUIstate.Data;
+        public static void Load(bool bPasswordNeeded)
+        {
+            //GUIstate.Data = GUIstate.Data;
 
-// ----------------------------------------------------------------------------
-// Get the name of the folder where all the game files will be stored. This
-// is placed in the Registry by the Race Generator and we cache a copy in the
-// GUI state data store.
-//
-// Note if the same folder on the same machine is chosen for all Nova GUI and
-// Nova Console files it is possible to play a game with multiple races using
-// the same log-in name. This is a useful debugging aid.
-// ----------------------------------------------------------------------------
+            // ----------------------------------------------------------------------------
+            // Get the name of the folder where all the game files will be stored. This
+            // is placed in the Registry by the Race Generator and we cache a copy in the
+            // GUI state data store.
+            //
+            // Note if the same folder on the same machine is chosen for all Nova GUI and
+            // Nova Console files it is possible to play a game with multiple races using
+            // the same log-in name. This is a useful debugging aid.
+            // ----------------------------------------------------------------------------
 
-         RegistryKey regKey    = Registry.CurrentUser;
-         RegistryKey regSubKey = regKey.CreateSubKey(Global.RootRegistryKey);
-         string gameFolder ="none";
-         try {
-            gameFolder = regSubKey.GetValue (Global.ClientFolderKey).ToString();
-         }
-         catch {
-            Report.FatalError("An expected registry entry is missing\n" +
-                              "Have you ran the Race Designer and \n" +
-                              "Nova Console?");
-          }
-
-// ----------------------------------------------------------------------------
-// Load the game persistent data and all component details and check the
-// password.
-// ----------------------------------------------------------------------------
-         if (bPasswordNeeded) //i.e. the first time the gui loads select a race..
-         {
-            GUIstate.Restore(gameFolder);
-         }
-         else 
-         {
-            GUIstate.Restore(gameFolder,GUIstate.Data.RaceName);
-         }
-
-         if (AllComponents.Restore() == false) {
-            Report.FatalError("Could not find component definition file");
-         }
-
-         if (GUIstate.Data.FirstTurn) {
-            GUIstate.Data.BattlePlans.Add("Default", new BattlePlan());
-            ProcessRaceDefinition();
-         }
-
-// ----------------------------------------------------------------------------
-// Check the password for access to this race's data
-// ----------------------------------------------------------------------------
-
-         ControlLibrary.CheckPassword password = 
-            new ControlLibrary.CheckPassword(GUIstate.Data.RaceData);
-
-         if (bPasswordNeeded)
-         {
-            password.ShowDialog();
-            password.Dispose();
-
-            if (password.DialogResult == DialogResult.Cancel)
+            RegistryKey regKey = Registry.CurrentUser;
+            RegistryKey regSubKey = regKey.CreateSubKey(Global.RootRegistryKey);
+            string gameFolder = "none";
+            try
             {
-               System.Threading.Thread.CurrentThread.Abort();
+                gameFolder = regSubKey.GetValue(Global.ClientFolderKey).ToString();
             }
-         }
-
-         LoadTurnFile();
-
-         if (GUIstate.Data.FirstTurn) 
-         {
-            foreach (string raceName in GUIstate.Data.InputTurn.AllRaceNames) 
+            catch
             {
-               GUIstate.Data.PlayerRelations[raceName] = "Neutral";
+                Report.FatalError("An expected registry entry is missing\n" +
+                                  "Have you ran the Race Designer and \n" +
+                                  "Nova Console?");
             }
-         }
 
-         GUIstate.Data.FirstTurn = false;
-      }
+            // ----------------------------------------------------------------------------
+            // Load the game persistent data and all component details and check the
+            // password.
+            // ----------------------------------------------------------------------------
+            if (bPasswordNeeded) //i.e. the first time the gui loads select a race..
+            {
+                GUIstate.Restore(gameFolder);
+            }
+            else
+            {
+                GUIstate.Restore(gameFolder, GUIstate.Data.RaceName);
+            }
 
+            if (AllComponents.Restore() == false)
+            {
+                Report.FatalError("Could not find component definition file");
+            }
 
-// ============================================================================
-// Read and process the Global Turn file (Nova.turn) generated by the Nova
-// Console. This file must be present before the GUI will run (since it
-// contains all sorts of important data such as a list of all stars which is
-// needed to draw the star map). This is also saved in the GUI state data
-// store.
-//
-// Note that this file is not read again after the first time a new turn is
-// received until a new turn arrives. This is because we will update some of
-// the Global Turn data during the preparation of the next turn.
-// ============================================================================
+            if (GUIstate.Data.FirstTurn)
+            {
+                GUIstate.Data.BattlePlans.Add("Default", new BattlePlan());
+                ProcessRaceDefinition();
+            }
 
-      public static void LoadTurnFile()
-      {
-         string turnFileName = Path.Combine(GUIstate.Data.GameFolder, "Nova.turn");
+            // ----------------------------------------------------------------------------
+            // Check the password for access to this race's data
+            // ----------------------------------------------------------------------------
 
-         if (File.Exists(turnFileName) == false) {
-            Report.FatalError
-            ("The Nova GUI cannot start unless a turn file is present");
-         }
+            ControlLibrary.CheckPassword password =
+               new ControlLibrary.CheckPassword(GUIstate.Data.RaceData);
 
-         FileStream turnFile = new FileStream(turnFileName, FileMode.Open);
-         int turnYearInFile  = (int) Formatter.Deserialize(turnFile);
-            
-         if (turnYearInFile != GUIstate.Data.TurnYear) {
-            GUIstate.Data.InputTurn = Formatter.Deserialize(turnFile)
-                                      as GlobalTurn;
-            turnFile.Close();
+            if (bPasswordNeeded)
+            {
+                password.ShowDialog();
+                password.Dispose();
 
-            GUIstate.Data.TurnYear = turnYearInFile;
-            GUIstate.Data.DeletedFleets.Clear();
-            GUIstate.Data.DeletedDesigns.Clear();
+                if (password.DialogResult == DialogResult.Cancel)
+                {
+                    System.Threading.Thread.CurrentThread.Abort();
+                }
+            }
 
-            InputTurnData.Process();
-         }
-      }
+            LoadTurnFile();
 
+            if (GUIstate.Data.FirstTurn)
+            {
+                foreach (string raceName in GUIstate.Data.InputTurn.AllRaceNames)
+                {
+                    GUIstate.Data.PlayerRelations[raceName] = "Neutral";
+                }
+            }
 
-// ============================================================================
-// Read the race definition file into the persistent data store. If this is the
-// very first turn of a new game then process it's content to set up initial
-// race parameters (e.g. initial technology levels, etc.).
-// ============================================================================
-
-      private static void ProcessRaceDefinition()
-      {
-         string raceFileName = Path.Combine(GUIstate.Data.GameFolder, 
-                                            GUIstate.Data.RaceName + ".race");
-                                                       
-         FileStream  raceFile   = new FileStream(raceFileName, FileMode.Open);
-         GUIstate.Data.RaceData = Formatter.Deserialize(raceFile) 
-                                  as Race;
-         raceFile.Close();
-
-         MainWindow.nova.Text = "Nova - " + GUIstate.Data.RaceData.PluralName;
-
-         ProcessPrimaryTraits();
-         ProcessSecondaryTraits();
-      }
+            GUIstate.Data.FirstTurn = false;
+        }
 
 
-// ============================================================================
-// Determine the components available to this race according to the starting
-// tech levels. Race-specific adjustments will be made in ProcessPrimaryTraits.
-// This processing is only performed once per game.
-// ============================================================================
+        // ============================================================================
+        // Read and process the Global Turn file (Nova.turn) generated by the Nova
+        // Console. This file must be present before the GUI will run (since it
+        // contains all sorts of important data such as a list of all stars which is
+        // needed to draw the star map). This is also saved in the GUI state data
+        // store.
+        //
+        // Note that this file is not read again after the first time a new turn is
+        // received until a new turn arrives. This is because we will update some of
+        // the Global Turn data during the preparation of the next turn.
+        // ============================================================================
 
-      public static void DetermineAvailableComponents()
-      {
-         foreach (NovaCommon.Component component in 
-                  AllComponents.Data.Components.Values) {
-            if (GUIstate.Data.ResearchLevel >= component.RequiredTech) {
-               RaceComponents.Add(component, false);
-           }
-         }
-      }
+        public static void LoadTurnFile()
+        {
+            string turnFileName = Path.Combine(GUIstate.Data.GameFolder, "Nova.turn");
+
+            if (File.Exists(turnFileName) == false)
+            {
+                Report.FatalError
+                ("The Nova GUI cannot start unless a turn file is present");
+            }
+
+            FileStream turnFile = new FileStream(turnFileName, FileMode.Open);
+            int turnYearInFile = (int)Formatter.Deserialize(turnFile);
+
+            if (turnYearInFile != GUIstate.Data.TurnYear)
+            {
+                GUIstate.Data.InputTurn = Formatter.Deserialize(turnFile)
+                                          as GlobalTurn;
+                turnFile.Close();
+
+                GUIstate.Data.TurnYear = turnYearInFile;
+                GUIstate.Data.DeletedFleets.Clear();
+                GUIstate.Data.DeletedDesigns.Clear();
+
+                InputTurnData.Process();
+            }
+        }
 
 
-// ============================================================================
-// Process the Primary Traits for this race.
-// ============================================================================
+        // ============================================================================
+        // Read the race definition file into the persistent data store. If this is the
+        // very first turn of a new game then process it's content to set up initial
+        // race parameters (e.g. initial technology levels, etc.).
+        // ============================================================================
 
-      private static void ProcessPrimaryTraits()
-      {
-         switch (GUIstate.Data.RaceData.Type) 
-		 {
-         case "JackOfAllTrades" :
-            GUIstate.Data.ResearchLevel = new TechLevel(3);//set all to 3
-            //GUIstate.Data.ResearchLevel = new TechLevel(26); //set all to 26
-            AddComponent("Chameleon");//obsolete but will be rejected now...
-            break;
+        private static void ProcessRaceDefinition()
+        {
+            string raceFileName = Path.Combine(GUIstate.Data.GameFolder,
+                                               GUIstate.Data.RaceName + ".race");
 
-          case "HyperExpansion":
-             //GUIstate.Data.ResearchLevel = new TechLevel(26);
-             GUIstate.Data.ResearchLevel = new TechLevel(1); // set all to 1
-             AddComponent("Settler's Delight");
-             AddComponent("Mini-Coloniser");
-             break;
-         }
+            FileStream raceFile = new FileStream(raceFileName, FileMode.Open);
+            GUIstate.Data.RaceData = Formatter.Deserialize(raceFile)
+                                     as Race;
+            raceFile.Close();
+
+            MainWindow.nova.Text = "Nova - " + GUIstate.Data.RaceData.PluralName;
+
+            ProcessPrimaryTraits();
+            ProcessSecondaryTraits();
+        }
+
+
+        // ============================================================================
+        // Determine the components available to this race according to the starting
+        // tech levels. Race-specific adjustments will be made in ProcessPrimaryTraits.
+        // This processing is only performed once per game.
+        // ============================================================================
+
+        public static void DetermineAvailableComponents()
+        {
+            foreach (NovaCommon.Component component in
+                     AllComponents.Data.Components.Values)
+            {
+                if (GUIstate.Data.ResearchLevel >= component.RequiredTech)
+                {
+                    RaceComponents.Add(component, false);
+                }
+            }
+        }
+
+
+        // ============================================================================
+        // Process the Primary Traits for this race.
+        // ============================================================================
+
+        private static void ProcessPrimaryTraits()
+        {
+            switch (GUIstate.Data.RaceData.Type)
+            {
+                case "JackOfAllTrades":
+                    GUIstate.Data.ResearchLevel = new TechLevel(3);//set all to 3
+                    //GUIstate.Data.ResearchLevel = new TechLevel(26); //set all to 26
+                    //AddComponent("Chameleon Scanner");//obsolete but will be rejected now...
+                    break;
+
+                case "HyperExpansion":
+                    //GUIstate.Data.ResearchLevel = new TechLevel(26);
+                    GUIstate.Data.ResearchLevel = new TechLevel(1); // set all to 1
+                    AddComponent("Settler's Delight");
+                    AddComponent("Mini-Colony Ship");
+                    break;
+            }
 #if (DEBUG)
-             // Just for testing
-             GUIstate.Data.ResearchLevel = new TechLevel(26);
+            // Just for testing
+            GUIstate.Data.ResearchLevel = new TechLevel(26);
 #endif
 
-         DetermineAvailableComponents();
-      }
+            DetermineAvailableComponents();
+        }
 
 
-// ============================================================================
-// Add a named component, will now check if it exists
-// ============================================================================
+        // ============================================================================
+        // Add a named component, will now check if it exists
+        // ============================================================================
 
-      private static void AddComponent(string componentName)
-      {
-         if (ComponentExists(componentName))
-         {
-            GUIstate.Data.AvailableComponents[componentName] = AllComponents.Data.Components[componentName];
-         }
-         else
-         {
-            string s = "Error : " + componentName + " does not exist!";
-            MessageBox.Show(s);
-         }
-      }
+        private static void AddComponent(string componentName)
+        {
+            if (ComponentExists(componentName))
+            {
+                GUIstate.Data.AvailableComponents[componentName] = AllComponents.Data.Components[componentName];
+            }
+            else
+            {
+                string s = "Error: The " + componentName + " component does not exist!";
+                MessageBox.Show(s);
+            }
+        }
 
-      private static bool ComponentExists(string componentName)
-      {
-         return AllComponents.Data.Components.ContainsKey(componentName);
-      }
+        private static bool ComponentExists(string componentName)
+        {
+            return AllComponents.Data.Components.ContainsKey(componentName);
+        }
 
-// ============================================================================
-// Process the Secondary Traits for this race.
-// ============================================================================
+        // ============================================================================
+        // Process the Secondary Traits for this race.
+        // ============================================================================
 
-      private static void ProcessSecondaryTraits()
-      {
-         if (GUIstate.Data.RaceData.Traits.Contains("IFE")) {
-            int level = (int) GUIstate.Data.ResearchLevel.TechValues["Propulsion"];
-            level++;
-            GUIstate.Data.ResearchLevel.TechValues["Propulsion"] = level;
-         }
-      }
+        private static void ProcessSecondaryTraits()
+        {
+            if (GUIstate.Data.RaceData.Traits.Contains("IFE"))
+            {
+                int level = (int)GUIstate.Data.ResearchLevel.TechValues["Propulsion"];
+                level++;
+                GUIstate.Data.ResearchLevel.TechValues["Propulsion"] = level;
+            }
+        }
 
-   }
+    }
 }
