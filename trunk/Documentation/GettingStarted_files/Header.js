93c4d4a65e4bc1f5f9f380835eaddbcd5c728128
dan_vale <dan_vale@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2010-01-06 23:27:42 +0000

    Organised documentation into sub-directories.

--- trunk/Documentation/GettingStarted_files/Header.js
+++
@@ -1,4 +0,0 @@
-document.write("<span style=\"font-size: 14pt; color: #3399cc; font-family: Arial\">")
-document.write("<strong> Nova </strong>")
-document.write("</span>")
-document.write("<br /> <hr />")
