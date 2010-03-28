ee6fc674319cedc3d0f1ce5a416a2314cd876b47
dan_vale <dan_vale@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2010-03-28 01:14:52 +0000

    Added a try-catch to prevent the double click on launch bug from causing a crash, see issue #2974019. Split Hull and HullModule classes into separate files. Fixed an error that was causing ship modules to loose track of their components.

--- trunk/ServerState/OrderReader.cs
+++ trunk/ServerState/OrderReader.cs
@@ -54,73 +54,89 @@ namespace NovaServer
         /// <param name="race">The race who's orders are to be loaded.</param>
         private static void ReadPlayerTurn(Race race)
         {
-            string fileName = Path.Combine(StateData.GameFolder, race.Name + ".Orders");
-
-            if (File.Exists(fileName) == false)
+            Orders playerOrders;
+            try
             {
-                return;
-                // TODO (priority 5) move this to a warning when generating a new turn.
-                Report.Error("There is no turn file for the " + race.Name
-                                  + " race.\n\nYou may only generate the first "
-                                  + "turn of a game when all race turn files are "
-                                  + "present.");
-            }
+                string fileName = Path.Combine(StateData.GameFolder, race.Name + ".Orders");
 
-            // Load from a binary serialised file (old format).
-            /*
-            Orders playerOrders = new Orders();
-            FileStream turnFile = new FileStream(fileName, FileMode.Open);
-            playerOrders = Formatter.Deserialize(turnFile) as Orders;
-            turnFile.Close();
-            */
+                if (File.Exists(fileName) == false)
+                {
+                    return;
+                    // TODO (priority 5) move this to a warning when generating a new turn.
+                    Report.Error("There is no turn file for the " + race.Name
+                                      + " race.\n\nYou may only generate the first "
+                                      + "turn of a game when all race turn files are "
+                                      + "present.");
+                }
 
-            // Load from an xml file
-            
-            XmlDocument xmldoc = new XmlDocument();
-            FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-            GZipStream compressionStream = new GZipStream(fileStream, CompressionMode.Decompress);
+                // Load from a binary serialised file (old format).
+                /*
+                Orders playerOrders = new Orders();
+                FileStream turnFile = new FileStream(fileName, FileMode.Open);
+                playerOrders = Formatter.Deserialize(turnFile) as Orders;
+                turnFile.Close();
+                */
+
+                // Load from an xml file
+
+                XmlDocument xmldoc = new XmlDocument();
+                FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                GZipStream compressionStream = new GZipStream(fileStream, CompressionMode.Decompress);
 #if (DEBUG)
-            xmldoc.Load(fileName);  // uncompressed
+                xmldoc.Load(fileName);  // uncompressed
 #else
             xmldoc.Load(compressionStream); // compressed
 #endif
-            Orders playerOrders = new Orders(xmldoc.DocumentElement);
-            fileStream.Close();
-            
+                playerOrders = new Orders(xmldoc.DocumentElement);
+                fileStream.Close();
 
-            // Regardless of how it was loaded:
-            LinkOrderReferences(playerOrders);
-
-            // TODO (priority 4) - check we are on the right turn before processing, and perhaps flag when we have processed the orders
-            // TODO (priority 4) fine tune so the client can't modify things like a star's position, i.e. treat the data as orders only.
-            foreach (Design design in playerOrders.RaceDesigns.Values)
-            {
-                StateData.AllDesigns[design.Key] = design;
             }
-
-            foreach (string fleetKey in playerOrders.DeletedFleets)
+            catch (Exception e)
             {
-                StateData.AllFleets.Remove(fleetKey);
+                Report.Error(Environment.NewLine + "There was a problem reading in the orders for " + race.Name + Environment.NewLine + "Details: " + e.Message);
+                return;
             }
-
-            foreach (string designKey in playerOrders.DeletedDesigns)
+            try
             {
-                StateData.AllDesigns.Remove(designKey);
-            }
 
-            foreach (Fleet fleet in playerOrders.RaceFleets.Values)
+                // Regardless of how it was loaded:
+                LinkOrderReferences(playerOrders);
+
+                // TODO (priority 4) - check we are on the right turn before processing, and perhaps flag when we have processed the orders
+                // TODO (priority 4) fine tune so the client can't modify things like a star's position, i.e. treat the data as orders only.
+                foreach (Design design in playerOrders.RaceDesigns.Values)
+                {
+                    StateData.AllDesigns[design.Key] = design;
+                }
+
+                foreach (string fleetKey in playerOrders.DeletedFleets)
+                {
+                    StateData.AllFleets.Remove(fleetKey);
+                }
+
+                foreach (string designKey in playerOrders.DeletedDesigns)
+                {
+                    StateData.AllDesigns.Remove(designKey);
+                }
+
+                foreach (Fleet fleet in playerOrders.RaceFleets.Values)
+                {
+                    StateData.AllFleets[fleet.Key] = fleet;
+                }
+
+                // load the orders for each star. 
+                foreach (Star star in playerOrders.RaceStars)
+                {
+                    StateData.AllStars[star.Name] = star;
+                }
+
+                StateData.AllRaceData[race.Name] = playerOrders.PlayerData;
+                StateData.AllTechLevels[race.Name] = playerOrders.TechLevel;
+            }
+            catch (Exception e)
             {
-                StateData.AllFleets[fleet.Key] = fleet;
+                Report.Error(Environment.NewLine + "There was a problem processing the orders for " + race.Name + Environment.NewLine + "Details: " + e.Message);
             }
-
-            // load the orders for each star. 
-            foreach (Star star in playerOrders.RaceStars)
-            {
-                StateData.AllStars[star.Name] = star;
-            }
-
-            StateData.AllRaceData[race.Name] = playerOrders.PlayerData;
-            StateData.AllTechLevels[race.Name] = playerOrders.TechLevel;
         }
 
         /// <summary>
@@ -153,6 +169,19 @@ namespace NovaServer
                     star.ThisRace = StateData.AllRaces[star.Owner] as Race;
                 if (star.Starbase != null)
                     star.Starbase = playerOrders.RaceFleets[star.Starbase.FleetID] as Fleet;
+            }
+
+            // HullModule reference to a component
+            foreach (Design design in playerOrders.RaceDesigns.Values)
+            {
+                if (design.Type == "Ship")
+                {
+                    ShipDesign ship = design as ShipDesign;
+                    foreach (HullModule module in ((Hull)ship.ShipHull.Properties["Hull"]).Modules)
+                    {
+                        module.AllocatedComponent = AllComponents.Data.Components[module.AllocatedComponent.Name] as Component;
+                    }
+                }
             }
 
 
