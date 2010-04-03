62ff725e287797aa5c3325d9c557c6128f5a521b
dan_vale <dan_vale@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2010-04-03 02:39:27 +0000

    Headers and function comments for Component Editor updated/cleaned up.

--- ComponentEditor/DialogComponents/TechRequirements.cs
+++ ComponentEditor/DialogComponents/TechRequirements.cs
@@ -1,6 +1,6 @@
-// This file needs -*- c++ -*- mode
-// ============================================================================
 // Nova. (c) 2008 Ken Reed
+// (c) 2009, 2010, stars-nova
+// See https://sourceforge.net/projects/stars-nova/
 //
 // User control for accessing research tech levels.
 //
@@ -9,6 +9,7 @@
 // Software Foundation.
 // ============================================================================
 
+#region Using Statements
 using NovaCommon;
 using System;
 using System.Collections;
@@ -18,56 +19,54 @@ using System.Drawing;
 using System.Data;
 using System.Text;
 using System.Windows.Forms;
+#endregion
 
 namespace ComponentEditor
 {
-   public partial class TechRequirements : UserControl
-   {
+    public partial class TechRequirements : UserControl
+    {
 
-// ============================================================================
-// Construction
-// ============================================================================
-
-      public TechRequirements()
-      {
-         InitializeComponent();
-      }
-
-
-// ============================================================================
-// Get the tech levels from the control
-// ============================================================================
-
-      public TechLevel Value {
-         get { 
-            TechLevel techLevel = new TechLevel();
-
-            techLevel[TechLevel.ResearchField.Biotechnology] = (int)BioTechLevel.Value;
-            techLevel[TechLevel.ResearchField.Construction] = (int)ConstructionTechLevel.Value;
-            techLevel[TechLevel.ResearchField.Electronics] = (int)ElectronicsTechLevel.Value;
-            techLevel[TechLevel.ResearchField.Energy] = (int)EnergyTechLevel.Value;
-            techLevel[TechLevel.ResearchField.Propulsion] = (int)PropulsionTechLevel.Value;
-            techLevel[TechLevel.ResearchField.Weapons] = (int)WeaponsTechLevel.Value;
-
-            return techLevel;
-         }
+        /// <summary>
+        /// Construction
+        /// </summary>
+        public TechRequirements()
+        {
+            InitializeComponent();
+        }
 
 
-// ============================================================================
-// Set the tech levels in the control
-// ============================================================================
+        /// <summary>
+        /// Property for getting/setting the Tech levels in the control.
+        /// </summary>
+        public TechLevel Value
+        {
+            // Get the tech levels from the control
+            get
+            {
+                TechLevel techLevel = new TechLevel();
 
-         set { 
-            BioTechLevel.Value          = (int) value[TechLevel.ResearchField.Biotechnology];
-            ElectronicsTechLevel.Value  = (int) value[TechLevel.ResearchField.Electronics];
-            EnergyTechLevel.Value       = (int) value[TechLevel.ResearchField.Energy];
-            PropulsionTechLevel.Value   = (int) value[TechLevel.ResearchField.Propulsion];
-            WeaponsTechLevel.Value      = (int) value[TechLevel.ResearchField.Weapons];
-            ConstructionTechLevel.Value = (int) value[TechLevel.ResearchField.Construction]; 
-         }
+                techLevel[TechLevel.ResearchField.Biotechnology] = (int)BioTechLevel.Value;
+                techLevel[TechLevel.ResearchField.Construction]  = (int)ConstructionTechLevel.Value;
+                techLevel[TechLevel.ResearchField.Electronics]   = (int)ElectronicsTechLevel.Value;
+                techLevel[TechLevel.ResearchField.Energy]        = (int)EnergyTechLevel.Value;
+                techLevel[TechLevel.ResearchField.Propulsion]    = (int)PropulsionTechLevel.Value;
+                techLevel[TechLevel.ResearchField.Weapons]       = (int)WeaponsTechLevel.Value;
 
-      }
+                return techLevel;
+            }
 
 
-   }
-}
+            // Set the tech levels in the control     
+            set
+            {
+                BioTechLevel.Value          = (int)value[TechLevel.ResearchField.Biotechnology];
+                ElectronicsTechLevel.Value  = (int)value[TechLevel.ResearchField.Electronics];
+                EnergyTechLevel.Value       = (int)value[TechLevel.ResearchField.Energy];
+                PropulsionTechLevel.Value   = (int)value[TechLevel.ResearchField.Propulsion];
+                WeaponsTechLevel.Value      = (int)value[TechLevel.ResearchField.Weapons];
+                ConstructionTechLevel.Value = (int)value[TechLevel.ResearchField.Construction];
+            }
+
+        }
+    }//TechRequirements
+}//namespace
