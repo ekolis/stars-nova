e61dcf5a4ef9b8dcd0e7232807b5f3a60767aef8
dan_vale <dan_vale@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2010-04-04 22:21:34 +0000

    Fixed null reference exception in Component Editor when selecting File->New with no component definition loaded. Added a batch file for listing in code bug comments (requires GNU grep).

---
+++ ShowBugs.bat
@@ -0,0 +1,50 @@
+@echo This file requires GNU Grep
+@echo .
+@echo           Stars Nova in code Bug List
+@echo           ---------------------------
+@echo .
+@echo            Priority 9
+@echo            ----------
+@echo .
+grep -r -n "(priority 9)" .
+@echo .
+@echo            Priority 8
+@echo            ----------
+@echo .
+grep -r -n "(priority 8)" .
+@echo .
+@echo            Priority 7
+@echo            ----------
+@echo .
+grep -r -n "(priority 7)" .
+@echo .
+@echo            Priority 6
+@echo            ----------
+@echo .
+grep -r -n "(priority 6)" .
+@echo .
+@echo            Priority 5
+@echo            ----------
+@echo .
+grep -r -n "(priority 5)" .
+@echo .
+@echo            Priority 4
+@echo            ----------
+@echo .
+grep -r -n "(priority 4)" .
+@echo .
+@echo            Priority 3
+@echo            ----------
+@echo .
+grep -r -n "(priority 3)" .
+@echo .
+@echo            Priority 2
+@echo            ----------
+@echo .
+grep -r -n "(priority 2)" .
+@echo .
+@echo            Priority 1
+@echo            ----------
+@echo .
+grep -r -n "(priority 1)" .
+@echo .
