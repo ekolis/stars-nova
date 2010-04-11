796462d2d49775d7401ff2a02760f61bd824012e
evild00d <evild00d@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2010-04-11 04:35:27 +0000

    Linked in VersionInfo.cs in each project and made each AssemblyInfo.cs contain the same minimal set of attributes to allow general settings values to be configured in the globally accessible VersionInfo.cs.

---
+++ trunk/Nova Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+// ============================================================================
+// Nova. (c) 2005 Ken Reed
+// (c) 2009, 2010, stars-nova
+// See https://sourceforge.net/projects/stars-nova/
+//
+// This is free software. You can redistribute it and/or modify it under the
+// terms of the GNU General Public License version 2 as published by the Free
+// Software Foundation.
+// ============================================================================
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Nova Common")]
+[assembly: AssemblyDescription("Nova Common")]
+[assembly: AssemblyProduct("Nova Common")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ef679930-f4c9-4d62-9a6a-de29f8d328c0")]
---
+++ trunk/Nova GUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+// This file needs -*- c++ -*- mode
+// ============================================================================
+// Nova. (c) 2008 Ken Reed
+//
+// This is free software. You can redistribute it and/or modify it under the
+// terms of the GNU General Public License version 2 as published by the Free
+// Software Foundation.
+// ============================================================================
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Nova GUI")]
+[assembly: AssemblyDescription("The user interface to the game")]
+[assembly: AssemblyProduct("Nova GUI")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("14cd4564-836f-4934-8b12-74f399b0f7d5")]
