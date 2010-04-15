7a95118e7f4225959c3144aa7dd97c2ebf6c2581
evild00d <evild00d@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2010-04-15 21:33:52 +0000

    Updated export bat files with new projects.

--- deploy.bat
+++ deploy.bat
@@ -4,12 +4,17 @@ IF NOT EXIST script.conf.bat EXIT /B 2
 
 :: Copy release files to a common folder
 set COPYCMD=xcopy /Y /s
-%COPYCMD% %SRCDIR%\components.xml %DISTDIR%\
+%COPYCMD% %SRCDIR%\ComponentEditor\bin\Release\*.* %DISTDIR%\
+%COPYCMD% %SRCDIR%\NewGame\bin\Release\*.* %DISTDIR%\
+%COPYCMD% %SRCDIR%\"Nova AI"\bin\Release\*.* %DISTDIR%\
+%COPYCMD% %SRCDIR%\"Nova Console"\bin\Release\*.* %DISTDIR%\
+%COPYCMD% %SRCDIR%\"Nova GUI"\bin\Release\*.* %DISTDIR%\
+%COPYCMD% %SRCDIR%\NovaLauncher\bin\Release\*.* %DISTDIR%\
+%COPYCMD% %SRCDIR%\"Race Designer"\bin\Release\*.* %DISTDIR%\
+
+%COPYCMD% %SRCDIR%\*.txt %DISTDIR%\
 %COPYCMD% %SRCDIR%\Documentation\GettingStarted.html %DISTDIR%\
+%COPYCMD% %SRCDIR%\components.xml %DISTDIR%\
 %COPYCMD% %SRCDIR%\Graphics\*.* %DISTDIR%\Graphics\
-%COPYCMD% %SRCDIR%\ComponentEditor\bin\Release\*.* %DISTDIR%\ComponentEditor\
-%COPYCMD% %SRCDIR%\"Race Designer"\bin\Release\*.* %DISTDIR%\"Race Designer"\
-%COPYCMD% %SRCDIR%\"Nova GUI"\bin\Release\*.* %DISTDIR%\"Nova GUI"\
-%COPYCMD% %SRCDIR%\"Nova Console"\bin\Release\*.* %DISTDIR%\"Nova Console"\
 
 pause
