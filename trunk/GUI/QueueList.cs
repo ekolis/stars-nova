97e5a59a84aac0d604bdd63364b853513c2da1ef
dan_vale <dan_vale@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2010-05-01 05:07:52 +0000

    File and function comment updates (finished GUI).

--- trunk/GUI/QueueList.cs
+++ trunk/GUI/QueueList.cs
@@ -1,11 +1,31 @@
-// This file needs -*- c++ -*- mode
+#region Copyright Notice
 // ============================================================================
-// Nova. (c) 2008 Ken Reed
+// Copyright (C) 2008 Ken Reed
+// Copyright (C) 2009, 2010 stars-nova
 //
-// This is free software. You can redistribute it and/or modify it under the
-// terms of the GNU General Public License version 2 as published by the Free
-// Software Foundation.
-// ============================================================================
+// This file is part of Stars-Nova.
+// See <http://sourceforge.net/projects/stars-nova/>.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>
+// ===========================================================================
+#endregion
+
+#region Module Description
+// ===========================================================================
+// This module provides a control to display the production queue for the 
+// currently selected star system.
+// ===========================================================================
+#endregion
 
 using NovaCommon;
 using System;
@@ -15,28 +35,34 @@ using System.Windows.Forms;
 
 namespace Nova
 {
+    /// <summary>
+    /// Populate a list control with the details of a production queue.
+    /// </summary>
+    public static class QueueList
+    {
 
-   public static class QueueList
-   {
+        /// ----------------------------------------------------------------------------
+        /// <summary>
+        /// Populate a list control with the details of a production queue.
+        /// </summary>
+        /// <param name="listView">The <see cref="ListView"/> to display the queue in.</param>
+        /// <param name="toMake">The <see cref="ProductionQueue"/> to display.</param>
+        /// ----------------------------------------------------------------------------
+        public static void Populate(ListView listView, ProductionQueue toMake)
+        {
+            listView.Items.Clear();
+            listView.BeginUpdate();
 
-// ============================================================================
-// Populate a list control with the details of a production queue.
-// ============================================================================
+            foreach (ProductionQueue.Item i in toMake.Queue)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = i.Name;
+                item.SubItems.Add(i.Quantity.ToString(System.Globalization.CultureInfo.InvariantCulture));
 
-      public static void Populate(ListView listView, ProductionQueue toMake)
-      {
-         listView.Items.Clear();
-         listView.BeginUpdate();
+                listView.Items.Add(item);
+            }
 
-         foreach (ProductionQueue.Item i in toMake.Queue) {
-            ListViewItem item = new ListViewItem();
-            item.Text         = i.Name;
-            item.SubItems.Add(i.Quantity.ToString(System.Globalization.CultureInfo.InvariantCulture));
-
-            listView.Items.Add(item);
-         }
-
-         listView.EndUpdate();
-      }
-   }
+            listView.EndUpdate();
+        }
+    }
 }
--- trunk/GUI/Reports/PlanetReport.cs
+++ trunk/GUI/Reports/PlanetReport.cs
@@ -1,12 +1,30 @@
+#region Copyright Notice
 // ============================================================================
-// Nova. (c) 2008 Ken Reed
+// Copyright (C) 2008 Ken Reed
+// Copyright (C) 2009, 2010 stars-nova
 //
+// This file is part of Stars-Nova.
+// See <http://sourceforge.net/projects/stars-nova/>.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>
+// ===========================================================================
+#endregion
+
+#region Module Description
+// ===========================================================================
 // Planet summary report.
-//
-// This is free software. You can redistribute it and/or modify it under the
-// terms of the GNU General Public License version 2 as published by the Free
-// Software Foundation.
-// ============================================================================
+// ===========================================================================
+#endregion
 
 using System;
 using System.Collections;
@@ -23,133 +41,157 @@ using NovaClient;
 namespace Nova
 {
 
-// ============================================================================
-// Planet summary report dialog class
-// ============================================================================
+    /// <summary>
+    /// Planet summary report dialog class
+    /// </summary>
+    public partial class PlanetReport : Form
+    {
 
-   public partial class PlanetReport : Form
-   {
-      public PlanetReport()
-      {
-         InitializeComponent();
-      }
+        #region Construction
 
+        /// ----------------------------------------------------------------------------
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// ----------------------------------------------------------------------------
+        public PlanetReport()
+        {
+            InitializeComponent();
+        }
 
-// ============================================================================
-// Populate the display. We use unbound population because some of the fields
-// need a little logic to decode (we don't just have a bunch of strings).
-// ============================================================================
+        #endregion
 
-      private void OnLoad(object sender, EventArgs e)
-      {
-         const int numColumns = 12;
-         Race      race       = ClientState.Data.PlayerRace;
+        #region Event Methods
 
-         PlanetGridView.Columns[8].Name = "Minerals";
-         PlanetGridView.AutoSize        = true;
+        /// ----------------------------------------------------------------------------
+        /// <summary>
+        /// Populate the display. We use unbound population because some of the fields
+        /// need a little logic to decode (we don't just have a bunch of strings).
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// ----------------------------------------------------------------------------
+        private void OnLoad(object sender, EventArgs e)
+        {
+            const int numColumns = 12;
+            Race race = ClientState.Data.PlayerRace;
 
-         Hashtable allStars = ClientState.Data.InputTurn.AllStars;
-         
-         foreach (Star star in allStars.Values) {
-            if (star.Owner == race.Name) {
-               string[] row = new string[numColumns];
-               
-               string starbase = "-";
-               if (star.Starbase != null) {
-                  starbase = star.Starbase.Name;
-               }
-               
-               int  i = 0;
-               row[i++] = star.Name;
-               row[i++] = starbase;
-               row[i++] = star.Colonists.ToString(System.Globalization.CultureInfo.InvariantCulture);
-               row[i++] = star.Capacity(race).ToString(System.Globalization.CultureInfo.InvariantCulture);
-               row[i++] = Math.Ceiling(star.HabitalValue(race)*100).ToString(System.Globalization.CultureInfo.InvariantCulture);
-               row[i++] = star.Mines.ToString(System.Globalization.CultureInfo.InvariantCulture);
-               row[i++] = star.Factories.ToString(System.Globalization.CultureInfo.InvariantCulture);
+            PlanetGridView.Columns[8].Name = "Minerals";
+            PlanetGridView.AutoSize = true;
 
-               Defenses.ComputeDefenseCoverage(star);
-               row[i++] = Defenses.SummaryCoverage.ToString(System.Globalization.CultureInfo.InvariantCulture);
+            Hashtable allStars = ClientState.Data.InputTurn.AllStars;
 
-               NovaCommon.Resources resources = star.ResourcesOnHand;
-               StringBuilder text             = new StringBuilder();
+            foreach (Star star in allStars.Values)
+            {
+                if (star.Owner == race.Name)
+                {
+                    string[] row = new string[numColumns];
 
-               text.AppendFormat("{0} {1} {2}", (int) resources.Ironium,
-                                                (int) resources.Boranium,
-                                                (int) resources.Germanium);
-               
-               string energy = ((int) resources.Energy).ToString(System.Globalization.CultureInfo.InvariantCulture);
+                    string starbase = "-";
+                    if (star.Starbase != null)
+                    {
+                        starbase = star.Starbase.Name;
+                    }
 
-               row[i++] = text.ToString();
+                    int i = 0;
+                    row[i++] = star.Name;
+                    row[i++] = starbase;
+                    row[i++] = star.Colonists.ToString(System.Globalization.CultureInfo.InvariantCulture);
+                    row[i++] = star.Capacity(race).ToString(System.Globalization.CultureInfo.InvariantCulture);
+                    row[i++] = Math.Ceiling(star.HabitalValue(race) * 100).ToString(System.Globalization.CultureInfo.InvariantCulture);
+                    row[i++] = star.Mines.ToString(System.Globalization.CultureInfo.InvariantCulture);
+                    row[i++] = star.Factories.ToString(System.Globalization.CultureInfo.InvariantCulture);
 
-               resources = star.MineralConcentration;
-               text      = new StringBuilder();
+                    Defenses.ComputeDefenseCoverage(star);
+                    row[i++] = Defenses.SummaryCoverage.ToString(System.Globalization.CultureInfo.InvariantCulture);
 
-               text.AppendFormat("{0} {1} {2}", (int) resources.Ironium,
-                                                (int) resources.Boranium,
-                                                (int) resources.Germanium);
-               row[i++] = text.ToString();
-               row[i++] = energy;
+                    NovaCommon.Resources resources = star.ResourcesOnHand;
+                    StringBuilder text = new StringBuilder();
 
-               PlanetGridView.Rows.Add(row);
+                    text.AppendFormat("{0} {1} {2}", (int)resources.Ironium,
+                                                     (int)resources.Boranium,
+                                                     (int)resources.Germanium);
+
+                    string energy = ((int)resources.Energy).ToString(System.Globalization.CultureInfo.InvariantCulture);
+
+                    row[i++] = text.ToString();
+
+                    resources = star.MineralConcentration;
+                    text = new StringBuilder();
+
+                    text.AppendFormat("{0} {1} {2}", (int)resources.Ironium,
+                                                     (int)resources.Boranium,
+                                                     (int)resources.Germanium);
+                    row[i++] = text.ToString();
+                    row[i++] = energy;
+
+                    PlanetGridView.Rows.Add(row);
+                }
             }
-         }
 
-         PlanetGridView.AutoResizeColumns();
-      }
+            PlanetGridView.AutoResizeColumns();
+        }
 
 
-// ============================================================================
-// Override cell painting so we can colour the mineral types. Still to be
-// implemented properly.
-// ============================================================================
+        /// ----------------------------------------------------------------------------
+        /// <summary>
+        /// Override cell painting so we can colour the mineral types. Still to be
+        /// implemented properly.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// ----------------------------------------------------------------------------
+        private void PlanetGridView_CellPainting(object sender,
+                     DataGridViewCellPaintingEventArgs e)
+        {
+            if (e.RowIndex < 0 || e.ColumnIndex < 0)
+            {
+                return;
+            }
 
-       private void PlanetGridView_CellPainting(object sender, 
-                    DataGridViewCellPaintingEventArgs e)
-       {
-          if (e.RowIndex < 0 || e.ColumnIndex < 0) {
-             return;
-          }
+            int mineralsIndex = PlanetGridView.Columns["Minerals"].Index;
 
-          int mineralsIndex = PlanetGridView.Columns["Minerals"].Index;
+            if (e.ColumnIndex != mineralsIndex)
+            {
+                return;
+            }
 
-          if (e.ColumnIndex != mineralsIndex) {
-             return;
-          }
+            Rectangle newRect = new Rectangle(e.CellBounds.X + 1,
+                      e.CellBounds.Y + 1, e.CellBounds.Width - 4,
+                      e.CellBounds.Height - 4);
 
-           Rectangle newRect = new Rectangle(e.CellBounds.X + 1,
-                     e.CellBounds.Y + 1, e.CellBounds.Width - 4,
-                     e.CellBounds.Height - 4);
+            Brush gridBrush = new SolidBrush(PlanetGridView.GridColor);
+            Brush backColorBrush = new SolidBrush(e.CellStyle.BackColor);
 
-           Brush gridBrush      = new SolidBrush(PlanetGridView.GridColor);
-           Brush backColorBrush = new SolidBrush(e.CellStyle.BackColor);
+            Pen gridLinePen = new Pen(gridBrush);
 
-           Pen gridLinePen = new Pen(gridBrush);
-    
-           e.Graphics.FillRectangle(backColorBrush, e.CellBounds);
+            e.Graphics.FillRectangle(backColorBrush, e.CellBounds);
 
-          // Draw the grid lines (only the right and bottom lines;
-          // DataGridView takes care of the others).
+            // Draw the grid lines (only the right and bottom lines;
+            // DataGridView takes care of the others).
 
-           e.Graphics.DrawLine(gridLinePen, e.CellBounds.Left,
-                               e.CellBounds.Bottom - 1, e.CellBounds.Right - 1,
-                               e.CellBounds.Bottom - 1);
-          
-           e.Graphics.DrawLine(gridLinePen, e.CellBounds.Right - 1,
-                               e.CellBounds.Top, e.CellBounds.Right - 1,
-                               e.CellBounds.Bottom);
+            e.Graphics.DrawLine(gridLinePen, e.CellBounds.Left,
+                                e.CellBounds.Bottom - 1, e.CellBounds.Right - 1,
+                                e.CellBounds.Bottom - 1);
 
-          // Draw the text content of the cell.
-          
-          if (e.Value != null) {
-             e.Graphics.DrawString((String)e.Value, e.CellStyle.Font,
-             Brushes.Crimson, e.CellBounds.X + 2,
-             e.CellBounds.Y + 2, StringFormat.GenericDefault);
-          }
+            e.Graphics.DrawLine(gridLinePen, e.CellBounds.Right - 1,
+                                e.CellBounds.Top, e.CellBounds.Right - 1,
+                                e.CellBounds.Bottom);
 
-          e.Handled = true;
-       }
+            // Draw the text content of the cell.
 
-   }
+            if (e.Value != null)
+            {
+                e.Graphics.DrawString((String)e.Value, e.CellStyle.Font,
+                Brushes.Crimson, e.CellBounds.X + 2,
+                e.CellBounds.Y + 2, StringFormat.GenericDefault);
+            }
+
+            e.Handled = true;
+        }
+
+        #endregion
+
+    }
 }
 
