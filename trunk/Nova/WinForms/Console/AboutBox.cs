57deba11d9d06eb492221f6f7378ffee85870caf
evild00d <evild00d@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2010-05-09 03:22:17 +0000

    Merged WinForms projects into one project (Nova) to generate a single exe.

---
+++ trunk/Nova/WinForms/Console/AboutBox.cs
@@ -0,0 +1,190 @@
+#region Copyright Notice
+// ============================================================================
+// Copyright (C) 2008 Ken Reed
+// Copyright (C) 2009, 2010 The Stars-Nova Project
+//
+// This file is part of Stars! Nova.
+// See <http://sourceforge.net/projects/stars-nova/>.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>
+// ===========================================================================
+#endregion
+
+#region Module Description
+// ===========================================================================
+// This module displays an about box.
+// ===========================================================================
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Reflection;
+
+
+namespace NovaConsole
+{
+    /// <summary>
+    /// This module displays an about box.
+    /// </summary>
+    partial class AboutBox : Form
+    {
+        #region Construction
+
+        /// ----------------------------------------------------------------------------
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// ----------------------------------------------------------------------------
+        public AboutBox()
+        {
+            InitializeComponent();
+
+            this.logoPictureBox.Image = Nova.Properties.Resources.Nova;
+
+            //  Initialize txhe AboutBox to display the product information from
+            //  the assembly information.  Change assembly information settings
+            //  for your application through either: -
+            //  Project->Properties->Application->Assembly Information -
+            //  AssemblyInfo.cs
+
+            this.Text = String.Format("About {0}", AssemblyTitle);
+            this.labelProductName.Text = AssemblyProduct;
+            this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
+            this.labelCopyright.Text = AssemblyCopyright;
+            this.labelCompanyName.Text = AssemblyCompany;
+        }
+
+        #endregion
+
+        #region Assembly Attribute Accessors
+
+        /// ----------------------------------------------------------------------------
+        /// <summary>
+        /// Get the assembly title.
+        /// </summary>
+        /// ----------------------------------------------------------------------------
+        public string AssemblyTitle
+        {
+            get
+            {
+                // Get all Title attributes on this assembly
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                // If there is at least one Title attribute
+                if (attributes.Length > 0)
+                {
+                    // Select the first one
+                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    // If it is not an empty string, return it
+                    if (titleAttribute.Title != "")
+                        return titleAttribute.Title;
+                }
+                // If there was no Title attribute, or if the Title attribute was the empty string, return the .exe name
+                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+            }
+        }
+
+        /// ----------------------------------------------------------------------------
+        /// <summary>
+        /// Get the assembly version.
+        /// </summary>
+        /// ----------------------------------------------------------------------------
+        public string AssemblyVersion
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        /// ----------------------------------------------------------------------------
+        /// <summary>
+        /// Get the assemble description.
+        /// </summary>
+        /// ----------------------------------------------------------------------------
+        public string AssemblyDescription
+        {
+            get
+            {
+                // Get all Description attributes on this assembly
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+                // If there aren't any Description attributes, return an empty string
+                if (attributes.Length == 0)
+                    return "";
+                // If there is a Description attribute, return its value
+                return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+            }
+        }
+
+        /// ----------------------------------------------------------------------------
+        /// <summary>
+        /// Get the assembly product.
+        /// </summary>
+        /// ----------------------------------------------------------------------------
+        public string AssemblyProduct
+        {
+            get
+            {
+                // Get all Product attributes on this assembly
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                // If there aren't any Product attributes, return an empty string
+                if (attributes.Length == 0)
+                    return "";
+                // If there is a Product attribute, return its value
+                return ((AssemblyProductAttribute)attributes[0]).Product;
+            }
+        }
+
+        /// ----------------------------------------------------------------------------
+        /// <summary>
+        /// Get the assembly copyright.
+        /// </summary>
+        /// ----------------------------------------------------------------------------
+        public string AssemblyCopyright
+        {
+            get
+            {
+                // Get all Copyright attributes on this assembly
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+                // If there aren't any Copyright attributes, return an empty string
+                if (attributes.Length == 0)
+                    return "";
+                // If there is a Copyright attribute, return its value
+                return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+            }
+        }
+
+        /// ----------------------------------------------------------------------------
+        /// <summary>
+        /// Get the Assembly Company.
+        /// </summary>
+        /// ----------------------------------------------------------------------------
+        public string AssemblyCompany
+        {
+            get
+            {
+                // Get all Company attributes on this assembly
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+                // If there aren't any Company attributes, return an empty string
+                if (attributes.Length == 0)
+                    return "";
+                // If there is a Company attribute, return its value
+                return ((AssemblyCompanyAttribute)attributes[0]).Company;
+            }
+        }
+
+        #endregion
+    }
+}
