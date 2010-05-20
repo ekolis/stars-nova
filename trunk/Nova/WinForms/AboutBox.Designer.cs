5ac55b06b5971b8098a6314708dfbf602e34fe1a
evild00d <evild00d@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2010-05-20 22:22:42 +0000

    Incorporated the Creative Commons Attribution-ShareAlike 3.0 Unported license for content.

--- trunk/Nova/WinForms/AboutBox.Designer.cs
+++ trunk/Nova/WinForms/AboutBox.Designer.cs
@@ -53,8 +53,6 @@ namespace Nova.WinForms
           this.logoPictureBox = new System.Windows.Forms.PictureBox();
           this.labelProductName = new System.Windows.Forms.Label();
           this.labelVersion = new System.Windows.Forms.Label();
-          this.labelCopyright = new System.Windows.Forms.Label();
-          this.labelCompanyName = new System.Windows.Forms.Label();
           this.Description = new System.Windows.Forms.TextBox();
           this.okButton = new System.Windows.Forms.Button();
           this.tableLayoutPanel.SuspendLayout();
@@ -69,19 +67,15 @@ namespace Nova.WinForms
           this.tableLayoutPanel.Controls.Add(this.logoPictureBox, 0, 0);
           this.tableLayoutPanel.Controls.Add(this.labelProductName, 1, 0);
           this.tableLayoutPanel.Controls.Add(this.labelVersion, 1, 1);
-          this.tableLayoutPanel.Controls.Add(this.labelCopyright, 1, 2);
-          this.tableLayoutPanel.Controls.Add(this.labelCompanyName, 1, 3);
-          this.tableLayoutPanel.Controls.Add(this.Description, 1, 4);
-          this.tableLayoutPanel.Controls.Add(this.okButton, 1, 5);
+          this.tableLayoutPanel.Controls.Add(this.Description, 1, 2);
+          this.tableLayoutPanel.Controls.Add(this.okButton, 1, 3);
           this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
           this.tableLayoutPanel.Location = new System.Drawing.Point(9, 9);
           this.tableLayoutPanel.Name = "tableLayoutPanel";
-          this.tableLayoutPanel.RowCount = 6;
+          this.tableLayoutPanel.RowCount = 4;
           this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
           this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-          this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-          this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-          this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+          this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
           this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
           this.tableLayoutPanel.Size = new System.Drawing.Size(417, 265);
           this.tableLayoutPanel.TabIndex = 0;
@@ -121,30 +115,6 @@ namespace Nova.WinForms
           this.labelVersion.TabIndex = 0;
           this.labelVersion.Text = "Version";
           this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-          // 
-          // labelCopyright
-          // 
-          this.labelCopyright.Dock = System.Windows.Forms.DockStyle.Fill;
-          this.labelCopyright.Location = new System.Drawing.Point(143, 52);
-          this.labelCopyright.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
-          this.labelCopyright.MaximumSize = new System.Drawing.Size(0, 17);
-          this.labelCopyright.Name = "labelCopyright";
-          this.labelCopyright.Size = new System.Drawing.Size(271, 17);
-          this.labelCopyright.TabIndex = 21;
-          this.labelCopyright.Text = "Copyright";
-          this.labelCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-          // 
-          // labelCompanyName
-          // 
-          this.labelCompanyName.Dock = System.Windows.Forms.DockStyle.Fill;
-          this.labelCompanyName.Location = new System.Drawing.Point(143, 78);
-          this.labelCompanyName.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
-          this.labelCompanyName.MaximumSize = new System.Drawing.Size(0, 17);
-          this.labelCompanyName.Name = "labelCompanyName";
-          this.labelCompanyName.Size = new System.Drawing.Size(271, 17);
-          this.labelCompanyName.TabIndex = 22;
-          this.labelCompanyName.Text = "Company Name";
-          this.labelCompanyName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
           // 
           // Description
           // 
@@ -199,8 +169,6 @@ namespace Nova.WinForms
       private System.Windows.Forms.PictureBox logoPictureBox;
       private System.Windows.Forms.Label labelProductName;
       private System.Windows.Forms.Label labelVersion;
-      private System.Windows.Forms.Label labelCopyright;
-      private System.Windows.Forms.Label labelCompanyName;
       private System.Windows.Forms.Button okButton;
       public System.Windows.Forms.TextBox Description;
    }
