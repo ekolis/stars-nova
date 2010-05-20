2c830cd2e21bf609cf244f50c9c2476bc323c2e1
evild00d <evild00d@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2010-05-20 22:22:42 +0000

    Incorporated the Creative Commons Attribution-ShareAlike 3.0 Unported license for content.

--- Nova/WinForms/AboutBox.cs
+++ Nova/WinForms/AboutBox.cs
@@ -34,38 +34,42 @@ namespace Nova.WinForms
 {
     partial class AboutBox : Form
     {
-        /// <summary>
-        /// Constructor
-        /// </summary>
         public AboutBox()
         {
             InitializeComponent();
 
             this.logoPictureBox.Image = Nova.Properties.Resources.Nova;
 
-            //  Initialize the AboutBox to display the product information from
-            //  the assembly information.  Change assembly information settings
-            //  for your application through either: -
-            //  Project->Properties->Application->Assembly Information -
-            //  AssemblyInfo.cs
-
             this.Text = String.Format("About {0}", AssemblyTitle);
             this.labelProductName.Text = AssemblyProduct;
             this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
-            this.labelCopyright.Text = AssemblyCopyright;
-            this.labelCompanyName.Text = AssemblyCompany;
             this.Description.Text =
-                "This program is free software; you can redistribute it and/or modify " +
-                "it under the terms of the GNU General Public License version 2 as " +
-                "published by the Free Software Foundation." + Environment.NewLine +
-                Environment.NewLine +
-                "This program is distributed in the hope that it will be useful, " +
-                "but WITHOUT ANY WARRANTY; without even the implied warranty of " +
-                "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the " +
-                "GNU General Public License for more details. " + Environment.NewLine +
-                Environment.NewLine +
-                "You should have received a copy of the GNU General Public License " +
-                "along with this program. If not, see <http://www.gnu.org/licenses/>";
+                "Copyright © 2008 Ken Reed" + Environment.NewLine +
+                "Copyright © 2009, 2010 The Stars-Nova Project" + Environment.NewLine +
+                "" + Environment.NewLine +
+                AssemblyProduct + " is licensed under two separate licenses for code and content." + Environment.NewLine +
+                "" + Environment.NewLine +
+                "Content (images, documentation and other media) is licensed under the " +
+                "Creative Commons Attribution-ShareAlike 3.0 Unported license. Content " +
+                "includes images, music, sounds, text and game content such as components " +
+                "and races. You should have received a copy of the Creative Commons " +
+                "Attribution-ShareAlike 3.0 Unported license along with this program. " +
+                "If not, visit <http://creativecommons.org/licenses/by-sa/3.0/> or send a " +
+                "letter to Creative Commons, 559 Nathan Abbott Way, Stanford, " +
+                "California 94305, USA." + Environment.NewLine +
+                "" + Environment.NewLine +
+                "Everything else is licensed under the GNU General Public License " +
+                "version 2. This includes, but is not limited to, source code, executable " +
+                "and object code. You should have received a copy of the GNU General " +
+                "Public License version 2 along with this program. If not, visit " +
+                "<http://www.gnu.org/licenses/> or send a letter to the " +
+                "Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, " +
+                "MA 02110-1301, USA." + Environment.NewLine +
+                "" + Environment.NewLine +
+                "This program is distributed in the hope that it will be useful, but " +
+                "WITHOUT ANY WARRANTY; without even the implied warranty of " +
+                "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General " +
+                "Public License version 2 for more details.";
         }
 
         #region Assembly Attribute Accessors
