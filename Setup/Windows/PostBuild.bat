1d8bd42fca670b58f97c7549d6427f853c1d62da
evild00d <evild00d@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2010-05-26 23:09:56 +0000

    Better error logging on setup post-build and removed manual setup file rename command.

--- Setup/Windows/PostBuild.bat
+++ Setup/Windows/PostBuild.bat
@@ -1,11 +1,10 @@
 @echo off
 SET LOGFILE=post-build.log
-echo Post-build script running > %LOGFILE%
+echo Running Post-build Scripts > %LOGFILE%
 echo ProjectDir = %1 >> %LOGFILE%
 echo Configuration = %2 >> %LOGFILE%
 echo BuiltOuputPath = %3 >> %LOGFILE%
 
-cscript.exe %1AddVersionToFileName.vbs %1 %2 %3 >> %LOGFILE%
+cscript.exe %1AddVersionToFileName.vbs %1 %2 %3 >> %LOGFILE% 2>&1
 
-del %1%2\setup.exe >> post-build.log >> %LOGFILE%
-move %3 %1%2\stars-nova-0.1.2-windows.msi >> %LOGFILE%
+del %1%2\setup.exe >> post-build.log >> %LOGFILE% 2>&1
