354fd9f1a25615eb8b708e9ff03ce176fd9a1c4d
evild00d <evild00d@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2010-05-31 16:51:28 +0000

    Removed the ability to resize the launcher window.

--- trunk/Nova/WinForms/Launcher/NovaLauncer.Designer.cs
+++ trunk/Nova/WinForms/Launcher/NovaLauncer.Designer.cs
@@ -162,7 +162,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.versionNumber);
             this.Controls.Add(this.versionLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "NovaLauncher";
             this.Text = "Nova Launcher";
             this.ResumeLayout(false);
