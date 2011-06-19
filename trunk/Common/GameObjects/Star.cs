e5e93dcb0ca053a131ee99a5f2100dd9c9699119
morsen <morsen@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2011-06-19 14:28:04 +0000

    Context menu select for fleets/planets

--- trunk/Common/GameObjects/Star.cs
+++ trunk/Common/GameObjects/Star.cs
@@ -86,6 +86,7 @@ namespace Nova.Common
             this.ManufacturingQueue = new ProductionQueue();
             this.MineralConcentration = new Resources();
             this.ResourcesOnHand = new Resources();
+            Type = "Star";
         }
 
         #endregion
