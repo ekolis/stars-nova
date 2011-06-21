0e059321b3aff18c33c6871da0132ddc550520bd
dan_vale <dan_vale@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2011-06-21 09:52:41 +0000

    Removed NewGame target from batch compile.

--- compile.bat
+++ compile.bat
@@ -1,6 +1,5 @@
 c:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe nova.sln /target:ClientState
 c:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe nova.sln /target:Common
 c:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe nova.sln /target:ControlLibrary
-c:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe nova.sln /target:NewGame
 c:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe nova.sln /target:Server
 
