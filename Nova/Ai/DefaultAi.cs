088f9a5994825e42cdd339083ed9b847ce3b068d
dan_vale <dan_vale@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2011-06-26 06:25:56 +0000

    Fixed an issue that caused the AI to crash due to accessing an illegal index for ResearchField. Incidentally fixed a logic issue that caused the AI to keep researching the same field every turn.

--- Nova/Ai/DefaultAi.cs
+++ Nova/Ai/DefaultAi.cs
@@ -77,19 +77,11 @@ namespace Nova.Ai
         }
         public override void DoMove()
         {
-            try
-            {
-                turnData = stateData.InputTurn;
+            turnData = stateData.InputTurn;
 
-                HandleProduction();
-                HandleResearch();
-                HandleMovements();
-
-            }
-            catch (Exception)
-            {
-                Report.FatalError("AI failed to take proper actions.");
-            }
+            HandleProduction();
+            HandleResearch();
+            HandleMovements();
         }
 
         private void HandleMovements()
@@ -181,14 +173,17 @@ namespace Nova.Ai
                 if (msg.Text.Contains("Your race has advanced to Tech Level") == true)
                 {
                     int minLevel = int.MaxValue;
-                    Nova.Common.TechLevel.ResearchField rs = TechLevel.ResearchField.Electronics;
-                    foreach (Nova.Common.TechLevel.ResearchField t in stateData.ResearchLevels)
+                    Nova.Common.TechLevel.ResearchField targetResearchField = TechLevel.ResearchField.Weapons; // default to researching weapons
+                    for (TechLevel.ResearchField field = TechLevel.FirstField; field < TechLevel.LastField; field++)
                     {
-                        minLevel = Math.Min(minLevel, stateData.ResearchLevels[t]);
-                        rs = t;
+                        if (stateData.ResearchLevels[field] < minLevel)
+                        {
+                            minLevel = stateData.ResearchLevels[field];
+                            targetResearchField = field;
+                        }
                     }
                     stateData.ResearchTopics.Zero();
-                    stateData.ResearchTopics[rs] = 1;                        
+                    stateData.ResearchTopics[targetResearchField] = 1;                        
                 }
             }
            
