afaa0e3c2997342e6200813be27cc20f316d8811
morsen <morsen@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2011-07-16 10:39:39 +0000

    Add rename fleet dialog

---
+++ trunk/Nova/WinForms/Gui/Dialogs/RenameFleetDialog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Nova.WinForms.Gui.Dialogs
+{
+    public partial class RenameFleetDialog : Form
+    {
+        public RenameFleetDialog()
+        {
+            InitializeComponent();
+        }
+
+        public string FleetName
+        {
+            get { return txtNewName.Text.Trim(); }
+
+            set 
+            { 
+                lblFleetName.Text = value;
+                txtNewName.Text = value;
+            } 
+        }
+
+        private void RenameFleetDialog_Load(object sender, EventArgs e)
+        {
+            txtNewName.SelectAll();
+            txtNewName.Focus();
+        }
+
+        private void txtNewName_TextChanged(object sender, EventArgs e)
+        {
+            btnOk.Enabled = txtNewName.Text.Trim().Length > 0;
+        }
+    }
+}
