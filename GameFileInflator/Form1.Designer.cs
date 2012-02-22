b8500785bef2493cd9f69659e07a730601aa31d6
dan_vale <dan_vale@03d337be-0f81-4a3b-8898-4a24ff344f0f>
2012-02-22 07:58:14 +0000

    re-added the GameFileInflator project as I find it easier than using standard zip tools

---
+++ GameFileInflator/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace GameFileInflator
+{
+    public partial class GameFileInflator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameFileInflator));
+            this.gameFilesLocationTextBox = new System.Windows.Forms.TextBox();
+            this.gameFilesBrowseButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.progressBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.decompressButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gameFilesLocationTextBox
+            // 
+            this.gameFilesLocationTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.gameFilesLocationTextBox.Location = new System.Drawing.Point(6, 22);
+            this.gameFilesLocationTextBox.Name = "gameFilesLocationTextBox";
+            this.gameFilesLocationTextBox.Size = new System.Drawing.Size(485, 20);
+            this.gameFilesLocationTextBox.TabIndex = 0;
+            // 
+            // gameFilesBrowseButton
+            // 
+            this.gameFilesBrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.gameFilesBrowseButton.Location = new System.Drawing.Point(497, 20);
+            this.gameFilesBrowseButton.Name = "gameFilesBrowseButton";
+            this.gameFilesBrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.gameFilesBrowseButton.TabIndex = 2;
+            this.gameFilesBrowseButton.Text = "Browse";
+            this.gameFilesBrowseButton.UseVisualStyleBackColor = true;
+            this.gameFilesBrowseButton.Click += new System.EventHandler(this.GameFilesBrowseButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.gameFilesLocationTextBox);
+            this.groupBox1.Controls.Add(this.gameFilesBrowseButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(581, 57);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Game Files Location";
+            // 
+            // progressBox
+            // 
+            this.progressBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBox.Location = new System.Drawing.Point(6, 19);
+            this.progressBox.Multiline = true;
+            this.progressBox.Name = "progressBox";
+            this.progressBox.ReadOnly = true;
+            this.progressBox.Size = new System.Drawing.Size(569, 160);
+            this.progressBox.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.progressBox);
+            this.groupBox2.Location = new System.Drawing.Point(12, 134);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(581, 185);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Progress";
+            // 
+            // decompressButton
+            // 
+            this.decompressButton.Location = new System.Drawing.Point(250, 93);
+            this.decompressButton.Name = "decompressButton";
+            this.decompressButton.Size = new System.Drawing.Size(75, 23);
+            this.decompressButton.TabIndex = 6;
+            this.decompressButton.Text = "De-compress";
+            this.decompressButton.UseVisualStyleBackColor = true;
+            this.decompressButton.Click += new System.EventHandler(this.DecompressButton_Click);
+            // 
+            // GameFileInflator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 331);
+            this.Controls.Add(this.decompressButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GameFileInflator";
+            this.Text = "Game File Inflator";
+            this.Load += new System.EventHandler(this.GameFileInflator_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox gameFilesLocationTextBox;
+        private System.Windows.Forms.Button gameFilesBrowseButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox progressBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button decompressButton;
+    }
+}
+
---
+++ GameFileInflator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GameFileInflator
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new GameFileInflator());
+        }
+    }
+}
